35a99d72aa084f800ecf214de2bc4c8b07960fd2
sinan <haxsmith77@gmail.com>
2023-05-10 21:57:07 +1000

    Updates

--- worldlistprocessor.py
+++ worldlistprocessor.py
@@ -7,20 +7,22 @@ class WorldListProcessor:
             # extract the columns from the row
             columns = row.find_all('td')
 
-            # extract the data from the columns
-            world_id = int(columns[0].find('a')['id'].split('-')[2])
-            population = int(columns[1].text.strip().split()[0])
+            try:
+                world_id = int(columns[0].find('a')['id'].split('-')[2])
+                population = int(columns[1].text.strip().split()[0])
 
-            if world_id not in self.world_populations:
-                self.world_populations[world_id] = population
-            else:
-                diff = population - self.world_populations[world_id]
-                if diff >= 10:
-                    print(f'World {world_id} has increased by {diff} players')
+                if world_id not in self.world_populations:
+                    self.world_populations[world_id] = population
+                else:
+                    diff = population - self.world_populations[world_id]
+                    if diff >= 10:
+                        print(f'World {world_id} has increased by {diff} players')
 
-                self.world_populations[world_id] = population
+                    self.world_populations[world_id] = population
+            except (IndexError, ValueError) as e:
+                print(f"Error parsing population data for world {world_id}")
 
-            # check if the difference is greater than or equal to 1
+
 
 
         
