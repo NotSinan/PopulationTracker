4e83d1b5d2f9cbef746eb5393aebcd7edd5ba5c9
Sinan <haxsmith77@gmail.com>
2023-05-13 00:47:51 +1000

    Updated worldlistprocessor for readability.

--- worldlistprocessor.py
+++ worldlistprocessor.py
@@ -1,28 +1,24 @@
+from constants import COLOR_GREEN, COLOR_RED, COLOR_RESET
+
 class WorldListProcessor:
-    def __init__(self) -> None:
+    def __init__(self, threshold):
         self.world_populations = {}
-    
+        self.threshold = threshold
+
     def get_population(self, rows):
         for row in rows:
-            # extract the columns from the row
             columns = row.find_all('td')
-            
+
             try:
                 world_id = int(columns[0].find('a')['id'].split('-')[2])
                 population = int(columns[1].text.strip().split()[0])
                 member_world = str(columns[3].text)
+
                 if member_world == "Members":
-                    if world_id not in self.world_populations:
+                    if world_id not in self.world_populations or self.world_populations[world_id] != population:
+                        diff = population - self.world_populations.get(world_id, 0)
+                        if diff >= self.threshold:
+                            print(f'{COLOR_GREEN} World {world_id} has increased by {diff} players.{COLOR_RESET}')
                         self.world_populations[world_id] = population
-                    else:
-                        diff = population - self.world_populations[world_id]
-                        if diff >= 5:
-                            print(f'World {world_id} has increased by {diff} players')
-                    self.world_populations[world_id] = population
-            except (IndexError, ValueError) as e:
+            except (IndexError, ValueError):
                 print(f"Error parsing population data for world {world_id}")
-
-
-
-
-        
