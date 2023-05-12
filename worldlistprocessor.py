0dc804c220b02d430bc962ade19d6520a098df79
Sinan <haxsmith77@gmail.com>
2023-05-12 23:30:21 +1000

    Added check for members world.

--- worldlistprocessor.py
+++ worldlistprocessor.py
@@ -11,7 +11,6 @@ class WorldListProcessor:
                 world_id = int(columns[0].find('a')['id'].split('-')[2])
                 population = int(columns[1].text.strip().split()[0])
                 member_world = str(columns[3].text)
-
                 if member_world == "Members":
                     if world_id not in self.world_populations:
                         self.world_populations[world_id] = population
@@ -19,7 +18,6 @@ class WorldListProcessor:
                         diff = population - self.world_populations[world_id]
                         if diff >= 5:
                             print(f'World {world_id} has increased by {diff} players')
-
                     self.world_populations[world_id] = population
             except (IndexError, ValueError) as e:
                 print(f"Error parsing population data for world {world_id}")
