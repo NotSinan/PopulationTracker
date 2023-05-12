d79055a849fec3415429fbc2018f5143f7ab76d0
Sinan <haxsmith77@gmail.com>
2023-05-13 00:47:36 +1000

    Added arguments via argparse

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from webscraper import WebScraper
 from worldlistprocessor import WorldListProcessor
 import time
-if __name__ == "__main__":
+import argparse
+
+def main(threshold):
     print("Application started")
     scraper = WebScraper("https://oldschool.runescape.com/slu?order=WMLPA")
-    wlp = WorldListProcessor()
+    wlp = WorldListProcessor(threshold)
 
     while True:
         rows = scraper.get_world_list_rows()
         wlp.get_population(rows)
         time.sleep(12)
     
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="OldSchool RuneScape Population Tracker.")
+    parser.add_argument('-t', '--threshold', type=int, default=5, help='Threshold for population difference')
+    args = parser.parse_args()
+    main(args.threshold)
