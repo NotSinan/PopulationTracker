0c279eb2ef92b6f630b724fc274461d516d3ede0
Sinan <haxsmith77@gmail.com>
2023-05-16 23:46:25 +1000

    Added a spinner to show progress.

--- populationtracker/main.py
+++ populationtracker/main.py
@@ -4,27 +4,28 @@ import time
 import argparse
 from prettyprinter import PrettyPrinter
 import pyfiglet
-import msvcrt
+from halo import Halo
 
 def main(threshold):
-    print(pyfiglet.figlet_format("OSRS Population Tracker"))
-    web_scraper = WebScraper("https://oldschool.runescape.com/slu?order=WMLPA")
-    world_list_processor = WorldListProcessor(threshold)
-    pretty_print = PrettyPrinter()
+    try:
+        print(pyfiglet.figlet_format("OSRS Population Tracker"))
+        web_scraper = WebScraper("https://oldschool.runescape.com/slu?order=WMLPA")
+        world_list_processor = WorldListProcessor(threshold)
+        pretty_print = PrettyPrinter()
+        spinner = Halo(text='Loading...', spinner='dots10', text_color="red", animation='marquee', color='red')
 
-    while True:
-        rows = web_scraper.get_world_list_rows()
-        pretty_print.prettyprint(world_list_processor.get_population(rows))
-        
-        # Check for user input to exit the program
-        if msvcrt.kbhit():
-            key = msvcrt.getch().decode()
-            if key.lower() == "q":
-                print("Exiting the program...")
-                break
+        while True:
+            rows = web_scraper.get_world_list_rows()
+            pretty_print.prettyprint(world_list_processor.get_population(rows))
+            spinner.text_color = "red"
+            with spinner.start():
+                time.sleep(10)
+                spinner.text_color = "green"
+                spinner.succeed("OK")
+    
+    except KeyboardInterrupt:
+        print("You have now exited the application.")
 
-        time.sleep(10)
-        
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="oldschool runescape population tracker")
     parser.add_argument('-t', '--threshold', type=int, default=5, help='threshold for population difference')
