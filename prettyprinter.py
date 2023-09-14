6fe41b7d9e1bc6d07ad9012d4082ebcf3a048644
sinan <haxsmith77@gmail.com>
2023-09-14 11:42:45 +1000

    Removed duplicates

--- prettyprinter.py
+++
@@ -1,6 +0,0 @@
-from tabulate import tabulate
-
-class PrettyPrinter():
-
-    def prettyprint(self, data):
-        print(tabulate(data, headers=['World', 'Change'], tablefmt='rounded_grid', stralign='center'))
